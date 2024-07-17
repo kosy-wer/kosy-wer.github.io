18aa3573b6f8365affd159851e745b85fa7c4832
kosy-wer <protectorunmatched@gmail.com>
2024-07-17 10:16:34 +0700

    kosy - email message = update

--- main.go
+++ main.go
@@ -15,6 +15,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// Handler untuk melayani file statis (gambar, CSS, JS, dll.)
+	fs := http.FileServer(http.Dir("."))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Handler untuk melayani halaman utama
 	http.HandleFunc("/", handler)
 	log.Println("Server running at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
